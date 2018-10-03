258c613688fe09dc7ad2ea65380605af8060f46e
Damon Hyde <damon.hyde@childrens.harvard.edu>
2018-10-03 14:23:54 -0400

    Improved subsref for labelledArray and derivatives

--- +MatTSA/@timeseries/subsref.m
+++ +MatTSA/@timeseries/subsref.m
@@ -9,21 +9,26 @@ switch s(1).type
       % Permits obj.data(<ref>) indexing.
       tmp = subsref(obj,s(1));
       s(2).subs = obj.getNumericIndex(s(2).subs{:});
-      varargout = {subsref(tmp,s(2:end))};      
+      %varargout = {subsref(tmp,s(2:end))};      
+      [varargout{1:nargout}] = subsref(tmp,s(2:end));
     elseif ismember(s(1).subs,{'addChannel' 'removeChannel'})
       % Bit of a hack. Is this really necessary? I think it should be
       % caught by the methods block of labelledArray.subsref.
       builtin('subsref',obj,s);
-      varargout = {obj};
+      %varargout = {obj};
+      [varargout{1:nargout}] = obj;
     else
-      varargout = {subsref@labelledArray(obj,s)};
+      %varargout = {subsref@labelledArray(obj,s)};
+      [varargout{1:nargout}] = subsref@labelledArray(obj,s);
     end
            
   case '()'
-    varargout = {subsref@labelledArray(obj,s)};
+    %varargout = {subsref@labelledArray(obj,s)};
+    [varargout{1:nargout}] = subsref@labelledArray(obj,s);
     
   case '{}'
-    varargout = {subsref@labelledArray(obj,s)};
+    %varargout = {subsref@labelledArray(obj,s)};
+    [varargout{1:nargout}] = subsref@labelledArray(obj,s);
     
   otherwise
     error('Not a valid indexing expression')
